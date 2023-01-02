840c748167f18fb2d848d1431cc733436c7aeea6
LIB <libcreatesweb3@gmail.com>
2023-01-02 18:03:40 +0900

    Add CSS

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -74,8 +74,57 @@ export default function Auth() {
         enableInterval(false)
     }
     return (
-        <div>
+        <div className={ContainerStyle}>
+            <h1 className={headerStyle}>Biconomy Auth Example</h1>
+            {
+                !smartAccount && !loading && 
+                <button className={buttonStyle} onClick={login}>Login</button>
+            }
+            {
+                loading && <p>Loading account details.....</p>
+            }
+            {
+                !!smartAccount && (
+                    <div className={detailsContainerStyle}>
+                        <h3>Smart account address:</h3>
+                        <p>{smartAccount.address}</p>
+                        <button className={buttonStyle} onClick={logout}></button>
+                    </div>
+                )
+            }
 
         </div>
     )
 }
+
+const detailsContainerStyle = css`
+  margin-top: 10px;
+`
+
+const buttonStyle = css`
+  padding: 14px;
+  width: 300px;
+  boarder: none;
+  cursor: pointer;
+  border-radius: 999px;
+  outline: none;
+  margin-top: 20px;
+  transition: all .25s;
+  &:hover {
+    background-color: rgba(0,0,0,.2);
+  }
+`
+
+const headerStyle = css`
+  font-size: 44px;
+`
+
+const ContainerStyle = css`
+  width: 900px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 100px;
+  
+`
